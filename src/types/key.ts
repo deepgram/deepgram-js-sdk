00edeee1c6f38973b96d5701b82faafb1746eef6
Luke Oliff <luke@lukeoliff.com>
2022-02-25 20:54:28 +0000

    fix: fix types to match current API out. fixes #25

--- src/types/key.ts
+++ src/types/key.ts
@@ -1,25 +1,43 @@
 /**
  * API key used for authenticating with the Deepgram API
  */
-export type Key = {
+export declare type Key = {
   /**
-   * Unique identifier of the key to use in API requests
+   * Api key member object
    */
-  api_key_id: string;
+  member: {
+    /**
+     * Unique identifier of the member on this API key
+     */
+    member_id: string;
+    /**
+     * Email of the member
+     */
+    email: string;
+  };
   /**
-   * API key to send in API requests (Only displayed when first created)
+   * Api key object
    */
-  key?: string;
-  /**
-   * Comment for user reference
-   */
-  comment: string;
-  /**
-   * Timestamp of the date/time the key was created
-   */
-  created: string;
-  /**
-   * Array of scopes assigned to the key
-   */
-  scopes: Array<string>;
+  api_key: {
+    /**
+     * Unique identifier of the key to use in API requests
+     */
+    api_key_id: string;
+    /**
+     * API key to send in API requests (Only displayed when first created)
+     */
+    key?: string;
+    /**
+     * Comment for user reference
+     */
+    comment: string;
+    /**
+     * Timestamp of the date/time the key was created
+     */
+    created: string;
+    /**
+     * Array of scopes assigned to the key
+     */
+    scopes: Array<string>;
+  };
 };
