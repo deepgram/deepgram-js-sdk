11e95f899486c299f7cb5d99854620dbfb88c1f6
Brian Barrow <briancbarrow@gmail.com>
2022-03-16 11:47:49 -0600

    added removeMember method for members endpoint

--- src/members.ts
+++ src/members.ts
@@ -1,6 +1,6 @@
 // import querystring from "querystring";
 import { _request } from "./httpRequest";
-import { MemberList } from "./types";
+import { MemberList, Message } from "./types";
 
 export class Members {
   constructor(private _credentials: string, private _apiUrl: string) {}
@@ -18,6 +18,21 @@ export class Members {
       this._credentials,
       this._apiUrl,
       `${this.apiPath}/${projectId}/members`
+    );
+  }
+
+  /**
+   * Retrieves account objects for all of the accounts in the specified project.
+   * @param projectId Unique identifier of the project
+   * @param memberId Unique identifier of the project
+   */
+  async removeMember(projectId: string, memberId: string): Promise<Message> {
+    console.log("REMOVE MEMBER", projectId, memberId);
+    return _request<Message>(
+      "DELETE",
+      this._credentials,
+      this._apiUrl,
+      `${this.apiPath}/${projectId}/members/${memberId}`
     );
   }
 }
