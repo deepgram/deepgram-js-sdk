8646b26d797099c3f6e22278766d88bb8df64f73
Brian Barrow <briancbarrow@gmail.com>
2022-03-16 15:04:31 -0600

    added update to scopes SDK functionality

--- src/scopes.ts
+++ src/scopes.ts
@@ -1,5 +1,5 @@
 import { _request } from "./httpRequest";
-import { ScopeList } from "./types";
+import { ScopeList, Message } from "./types";
 
 export class Scopes {
   constructor(private _credentials: string, private _apiUrl: string) {}
@@ -10,12 +10,28 @@ export class Scopes {
    * Retrieves account objects for all of the accounts in the specified project.
    * @param projectId Unique identifier of the project
    */
-  async getScopes(projectId: string, memberId: string): Promise<ScopeList> {
+  async get(projectId: string, memberId: string): Promise<ScopeList> {
     return _request<ScopeList>(
       "GET",
       this._credentials,
       this._apiUrl,
       `${this.apiPath}/${projectId}/members/${memberId}/scopes`
+    );
+  }
+
+  async update(
+    projectID: string,
+    memberId: string,
+    scope: string
+  ): Promise<Message> {
+    return _request<Message>(
+      "PUT",
+      this._credentials,
+      this._apiUrl,
+      `${this.apiPath}/${projectID}/members/${memberId}/scopes`,
+      JSON.stringify({
+        scope,
+      })
     );
   }
 }
