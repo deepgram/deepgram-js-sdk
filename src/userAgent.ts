23835ccfa0d3cf05ef9cf42f2b1d07d484ef3fb0
Michael Jolley <mike@sparcapp.io>
2022-03-31 22:13:09 -0500

    Continuing bundle work

--- src/userAgent.ts
+++ src/userAgent.ts
@@ -1,11 +1,13 @@
-import * as packageDetails from "../package.json";
+/*
+* The following const is replaced during the CD
+* process to the version from package.json
+*/
+const sdkVersion = "DG_SDK_VERSION";
 
 export function userAgent(): string {
   let agent = "@deepgram/sdk/UNKNOWN node/UNKNOWN";
   try {
-    agent = `@deepgram/sdk/${
-      packageDetails.version
-    } node/${process.version.replace("v", "")}`;
+    agent = `@deepgram/sdk/${sdkVersion} node/${process.version.replace("v", "")}`;
   } catch (e) {
     console.warn("Could not load package details");
   }
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    clean: true
+  },
+};
