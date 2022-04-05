bd923302f17de78b108f3f3e2c5f61db12bba8ab
Brian Barrow <briancbarrow@gmail.com>
2022-04-05 08:58:06 -0600

    added preRecorded method to browserified node sdk

--- src/browser/transcription/index.ts
+++ src/browser/transcription/index.ts
@@ -1,9 +1,31 @@
 import {
   LiveTranscriptionOptions,
+  PrerecordedTranscriptionOptions,
+  PrerecordedTranscriptionResponse,
+  UrlSource,
 } from "../../types";
 import querystring from "querystring";
+import { preRecordedTranscription } from "./preRecordedTranscription";
+
 export class Transcriber {
   constructor(private _credentials: string, private _apiUrl: string) {}
+
+  /**
+   * Transcribes prerecorded audio from a file or buffer
+   * @param source Url or Buffer of file to transcribe
+   * @param options Options to modify transcriptions
+   */
+  async preRecorded(
+    source: UrlSource,
+    options?: PrerecordedTranscriptionOptions
+  ): Promise<PrerecordedTranscriptionResponse> {
+    return await preRecordedTranscription(
+      this._credentials,
+      this._apiUrl || "",
+      source,
+      options
+    );
+  }
 
   /**
    * Opens a websocket to Deepgram's API for live transcriptions
