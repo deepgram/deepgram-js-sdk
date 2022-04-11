4167374624b03177c216761f9b805315c20dd1ce
Brian Barrow <briancbarrow@gmail.com>
2022-04-11 13:39:09 -0600

    brought in members support

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -4,7 +4,7 @@ import { Transcriber } from "./transcription";
 import { Projects } from "../projects";
 import { Keys } from "../keys";
 // import { Usage } from "../usage";
-// import { Members } from "../members";
+import { Members } from "../members";
 // import { Invitation } from "../invitation";
 // import { Billing } from "../billing";
 // import { Scopes } from "../scopes";
@@ -19,7 +19,7 @@ export class Deepgram {
   projects: Projects;
   keys: Keys;
   // usage: Usage;
-  // members: Members;
+  members: Members;
   // invitation: Invitation;
   // billing: Billing;
   // scopes: Scopes;
@@ -37,7 +37,7 @@ export class Deepgram {
     this.projects = new Projects(this._apiKey, this._apiUrl, _request);
     this.keys = new Keys(this._apiKey, this._apiUrl, _request);
     // this.usage = new Usage(this._apiKey, this._apiUrl);
-    // this.members = new Members(this._apiKey, this._apiUrl);
+    this.members = new Members(this._apiKey, this._apiUrl);
     // this.invitation = new Invitation(this._apiKey, this._apiUrl);
     // this.billing = new Billing(this._apiKey, this._apiUrl);
     // this.scopes = new Scopes(this._apiKey, this._apiUrl);
