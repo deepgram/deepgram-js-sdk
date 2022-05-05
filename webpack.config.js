470e102f83617f52cc761ef7d3503913dfe6d000
Brian Barrow <briancbarrow@gmail.com>
2022-05-05 16:53:56 -0600

    added webpack config to minimize shipped code

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  name: "server",
+  target: "node",
+  entry: "./bundle/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      name: "dg-node-sdk",
+      type: "umd",
+    },
+  },
+};
