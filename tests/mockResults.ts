19963abb32d55e14ecd549249eb9b44ff6140871
Luke Oliff <luke@lukeoliff.com>
2023-05-02 19:59:29 +0100

    redo prerecord tests for custom endpoint, add tests for prerecord

--- tests/mockResults.ts
+++ tests/mockResults.ts
@@ -1,8 +1,12 @@
+import { Readable } from "stream";
 import {
   Project,
   ProjectPatchRequest,
   ProjectResponse,
+  ReadStreamSource,
   ScopeList,
+  BufferSource,
+  UrlSource,
   UsageField,
   UsageFieldOptions,
   UsageOptions,
@@ -11,6 +15,15 @@ import {
   UsageRequestListOptions,
   UsageResponse,
   UsageResponseDetail,
+  PrerecordedTranscriptionOptions,
+  PrerecordedTranscriptionResponse,
+  Metadata,
+  Channel,
+  Utterance,
+  Search,
+  Hit,
+  Alternative,
+  WordBase,
 } from "../src/types";
 
 export const mockApiKey = "testKey";
@@ -211,4 +224,78 @@ export const mockProjects: ProjectResponse = {
 export const mockProjectUpdate: ProjectPatchRequest = {
   name: "string",
   company: "string",
+};
+
+export const mockReadable = new Readable();
+mockReadable._read = () => {};
+mockReadable.push("string");
+mockReadable.push(null);
+
+export const mockReadStreamSource: ReadStreamSource = {
+  stream: mockReadable,
+  mimetype: "video/mpeg",
+};
+
+export const mockUrlSource: UrlSource = {
+  url: "string",
+};
+
+export const mockBuffer: Buffer = Buffer.from("string");
+
+export const mockBufferSource: BufferSource = {
+  buffer: mockBuffer,
+  mimetype: "video/mpeg",
+};
+
+export const mockPrerecordedOptions: PrerecordedTranscriptionOptions = {
+  model: "nova",
+  punctuate: true,
+};
+
+export const mockMetaData: Metadata = {
+  request_id: "string",
+  transaction_key: "string",
+  sha256: "string",
+  created: "string",
+  duration: 1,
+  channels: 1,
+};
+
+export const mockHit: Hit = {
+  confidence: 1,
+  start: 1,
+  end: 1,
+  snippet: "string",
+};
+
+export const mockSearch: Search = {
+  query: "string",
+  hits: [mockHit],
+};
+
+export const mockWordBase: WordBase = {
+  word: "string",
+  start: 1,
+  end: 1,
+  confidence: 1,
+};
+
+export const mockAlternative: Alternative = {
+  transcript: "string",
+  confidence: 1,
+  words: [mockWordBase],
+};
+
+export const mockChannel: Channel = {
+  search: [mockSearch],
+  alternatives: [mockAlternative],
+  detected_language: "string",
+};
+
+export const mockPrerecordedResponse = {
+  request_id: mockUuid,
+  metadata: mockMetaData,
+  results: {
+    channels: [mockChannel],
+  },
 };
