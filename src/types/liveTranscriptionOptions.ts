a1c61cba829b80af5ebd1f848f326cbc917ce520
Luke Oliff <luke@lukeoliff.com>
2023-05-23 15:37:28 -0700

    correct format for transcription options

--- src/types/liveTranscriptionOptions.ts
+++ src/types/liveTranscriptionOptions.ts
@@ -253,13 +253,5 @@ export type LiveTranscriptionOptions = {
   /**
    * allow arbitrary unknown key/value pairs to be passed through the SDK to the API
    */
-  [key: string]:
-    | string
-    | number
-    | boolean
-    | readonly string[]
-    | readonly number[]
-    | readonly boolean[]
-    | null
-    | undefined;
+  [key: string]: unknown;
 };
