97b482be0743ce72632a31737955a0a42cc4055f
Brian Barrow <briancbarrow@gmail.com>
2023-05-24 15:32:58 -0600

    exposed LiveTranscription for Typescript

--- src/types/index.ts
+++ src/types/index.ts
@@ -46,4 +46,5 @@ export * from "./usageResponse";
 export * from "./usageResponseDetail";
 export * from "./utterance";
 export * from "./wordBase";
-export * from "./error"
+export * from "./error";
+export * from "../transcription/liveTranscription";
