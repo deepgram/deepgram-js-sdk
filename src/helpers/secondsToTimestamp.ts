28c3ccac888bb37d32fd9ee6f5e49a4676d50c13
Luke Oliff <luke@lukeoliff.com>
2023-06-20 16:07:28 +0100

    fix timestamp off-by-1-hour error

--- src/helpers/secondsToTimestamp.ts
+++ src/helpers/secondsToTimestamp.ts
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
 
 export function secondsToTimestamp(
   seconds: number,
   format = "HH:mm:ss.SSS"
 ): string {
-  return dayjs(seconds * 1000).format(format);
+  return dayjs(seconds * 1000)
+    .utc()
+    .format(format);
 }
