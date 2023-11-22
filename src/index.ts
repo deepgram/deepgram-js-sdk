a75db1f996af5f431dd426d452b42afc7c9db38a
Luke Oliff <luke@lukeoliff.com>
2023-11-22 15:01:57 +0000

    feat: update websocket to use w3cwebsocket for node/browser/edge worker examples (#205)

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,16 @@ export * from "./lib/types";
 export * from "./lib/enums";
 export * from "./lib/constants";
 export * from "./lib/errors";
+export * from "./lib/helpers";
 
 /**
- * Captions.
+ * Captions. These will be tree-shaken if unused.
+ *
+ * @see https://github.com/deepgram/deepgram-node-captions
+ *
+ * import/export declarations don't do anything but set up an alias to the
+ * exported variable, they do not count as a "use". Given their semantics,
+ * they are tracked specially by any bundler and will not adversely affect
+ * tree-shaking.
  */
 export { webvtt, srt } from "@deepgram/captions";
