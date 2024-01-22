a9fed0408992a1a790835438001d3286dd6feb87
Luke Oliff <luke@lukeoliff.com>
2024-01-22 16:01:34 +0000

    feat: sits analyze types and schema

--- src/lib/types/SyncPrerecordedResponse.ts
+++ src/lib/types/SyncPrerecordedResponse.ts
@@ -3,51 +3,6 @@ export interface SyncPrerecordedResponse {
   results: Result;
 }
 
-interface Result {
-  channels: Channel[];
-  utterances?: Utterance[];
-  summary?: TranscriptionSummary;
-  sentiments?: Sentiment;
-  topics?: Topics;
-  intents?: Intents;
-}
-
-interface Channel {
-  alternatives: Alternative[];
-  search?: Search[];
-  detected_language?: string;
-  language_confidence?: number;
-}
-
-interface Utterance {
-  start: number;
-  end: number;
-  confidence: number;
-  channel: number;
-  transcript: string;
-  words: WordBase[];
-  speaker?: number;
-  id: string;
-}
-
-interface TranscriptionSummary {
-  result: string;
-  short: string;
-}
-
-interface Sentiment {
-  segments?: SentimentSegment[];
-  average?: Average;
-}
-
-interface Topics {
-  segments?: TopicsSegment[];
-}
-
-interface Intents {
-  segments?: IntentsSegment[];
-}
-
 interface Alternative {
   transcript: string;
   confidence: number;
@@ -59,21 +14,11 @@ interface Alternative {
   topics?: TopicGroup[];
 }
 
-interface Search {
-  query: string;
-  hits: Hit[];
-}
-
-interface WordBase {
-  word: string;
-  start: number;
-  end: number;
-  confidence: number;
-  punctuated_word?: string;
-  speaker?: number;
-  speaker_confidence?: number;
-  sentiment: string;
-  sentiment_score: number;
+interface Channel {
+  search?: Search[];
+  alternatives: Alternative[];
+  detected_language?: string;
+  language_confidence?: number;
 }
 
 interface Entity {
@@ -99,14 +44,42 @@ interface Metadata {
   duration: number;
   channels: number;
   models: string[];
-  model_info: Record<string, ModelInfo>;
   warnings?: Warning[];
+  model_info: Record<string, ModelInfo>;
+  summary_info?: SummaryInfo;
+  intents_info?: IntentsInfo;
+  sentiment_info?: SentimentInfo;
+  topics_info?: TopicsInfo;
 }
 
 interface ModelInfo {
   name: string;
   version: string;
   arch: string;
+}
+
+interface SummaryInfo {
+  input_tokens: number;
+  output_tokens: number;
+  model_uuid: string;
+}
+
+interface IntentsInfo {
+  model_uuid: string;
+  input_tokens: number;
+  output_tokens: number;
+}
+
+interface SentimentInfo {
+  model_uuid: string;
+  input_tokens: number;
+  output_tokens: number;
+}
+
+interface TopicsInfo {
+  model_uuid: string;
+  input_tokens: number;
+  output_tokens: number;
 }
 
 interface Paragraph {
@@ -121,6 +94,58 @@ interface ParagraphGroup {
   paragraphs: Paragraph[];
 }
 
+interface Result {
+  channels: Channel[];
+  utterances?: Utterance[];
+  summary?: TranscriptionSummary;
+  sentiments?: Sentiments;
+  topics?: Topics;
+  intents?: Intents;
+}
+
+interface Sentiments {
+  segments: Segment[];
+  average: Average;
+}
+
+interface Topics {
+  segments: Segment[];
+}
+
+interface Intents {
+  segments: Segment[];
+}
+
+interface Intent {
+  intent: string;
+  confidence_score: number;
+}
+
+interface Average {
+  sentiment: string;
+  sentiment_score: number;
+}
+
+interface Topic {
+  topic: string;
+  confidence_score: number;
+}
+
+interface Segment {
+  text: string;
+  start_word: number;
+  end_word: number;
+  sentiment?: string;
+  sentiment_score?: number;
+  topics?: Topic[];
+  intents?: Intent[];
+}
+
+interface Search {
+  query: string;
+  hits: Hit[];
+}
+
 interface Sentence {
   text: string;
   start: number;
@@ -131,6 +156,10 @@ interface Summary {
   summary?: string;
   start_word?: number;
   end_word?: number;
+}
+interface TranscriptionSummary {
+  result: string;
+  short: string;
 }
 
 interface Topic {
@@ -150,35 +179,29 @@ interface Translation {
   translation: string;
 }
 
+interface Utterance {
+  start: number;
+  end: number;
+  confidence: number;
+  channel: number;
+  transcript: string;
+  words: WordBase[];
+  speaker?: number;
+  id: string;
+}
+
 interface Warning {
   parameter: string;
   type: string;
   message: string;
 }
 
-interface SentimentSegment {
-  text: string;
-  start_word: number;
-  end_word: number;
-  sentiment: "positive" | "neutral" | "negative";
-  sentiment_score: number;
-}
-
-interface Average {
-  sentiment: string;
-  sentiment_score: number;
-}
-
-interface TopicsSegment {
-  text: string;
-  start_word: number;
-  end_word: number;
-  topics: { topic: string; confidence_score: number }[];
-}
-
-interface IntentsSegment {
-  text: string;
-  start_word: number;
-  end_word: number;
-  intents: { intent: string; confidence_score: number }[];
+interface WordBase {
+  word: string;
+  start: number;
+  end: number;
+  confidence: number;
+  punctuated_word?: string;
+  speaker?: number;
+  speaker_confidence?: number;
 }
