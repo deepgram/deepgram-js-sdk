d8e6be8cf290bc26f582f23bb3554e0205caf104
Luke Oliff <luke@lukeoliff.com>
2024-01-24 14:00:52 +0000

    fix: allows endpointing to be disabled with a value of false

--- src/lib/types/TranscriptionSchema.ts
+++ src/lib/types/TranscriptionSchema.ts
@@ -147,7 +147,7 @@ interface LiveSchema extends TranscriptionSchema {
   /**
    * @see https://developers.deepgram.com/docs/endpointing
    */
-  endpointing?: number;
+  endpointing?: false | number;
 
   /**
    * @see https://developers.deepgram.com/docs/interim-results
