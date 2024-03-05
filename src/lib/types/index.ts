89488567f670ee436571495b5ca4884cfd8cbaa3
Sandra Rodgers <sandra.rodgers@deepgram.com>
2024-03-05 09:40:18 -0600

    feat: speak endpoint added

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -42,6 +42,7 @@ export type {
   AnalyzeSource,
 } from "./DeepgramSource";
 export type { SendProjectInviteSchema } from "./SendProjectInviteSchema";
+export type { SpeakSchema } from "./SpeakSchema";
 export type { SpeechStartedEvent } from "./SpeechStartedEvent";
 export type { SyncPrerecordedResponse } from "./SyncPrerecordedResponse";
 export type { SyncAnalyzeResponse } from "./SyncAnalyzeResponse";
