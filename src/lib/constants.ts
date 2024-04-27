e5ab6c8a8b44037b5eb330f9453f6139e172a811
Luke Oliff <luke@lukeoliff.com>
2024-04-27 11:49:41 +0100

    fix: update user agent to include JS or node (#269)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,10 +3,12 @@ import { DeepgramClientOptions } from "./types/DeepgramClientOptions";
 import { FetchOptions } from "./types/Fetch";
 import { version } from "./version";
 
+export const NODE_VERSION = process.versions.node;
+
 export const DEFAULT_HEADERS = {
   "Content-Type": `application/json`,
   "X-Client-Info": `@deepgram/sdk; ${isBrowser() ? "browser" : "server"}; v${version}`,
-  "User-Agent": `@deepgram/sdk/${version}`,
+  "User-Agent": `@deepgram/sdk/${version} ${isBrowser() ? "javascript" : `node/${NODE_VERSION}`}`,
 };
 
 export const DEFAULT_URL = "https://api.deepgram.com";
