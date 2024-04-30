a0664565b0c953dd479658b138000060000baebf
Luke Oliff <luke@lukeoliff.com>
2024-04-30 15:14:47 +0100

    fix: process is undefined error in vite

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,10 @@ import { DeepgramClientOptions } from "./types/DeepgramClientOptions";
 import { FetchOptions } from "./types/Fetch";
 import { version } from "./version";
 
-export const NODE_VERSION = process.versions.node;
+export const NODE_VERSION =
+  typeof process !== "undefined" && process.versions && process.versions.node
+    ? process.versions.node
+    : "unknown";
 
 export const DEFAULT_HEADERS = {
   "Content-Type": `application/json`,
