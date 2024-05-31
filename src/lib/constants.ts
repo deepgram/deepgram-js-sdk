c16643091bfb784cbeb089a156bb1a34348e20ac
Richard <richard@stromer.org>
2024-05-31 11:59:02 +0200

    fix: make sure process.versions without node key also works

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ import { DeepgramClientOptions } from "./types/DeepgramClientOptions";
 import { FetchOptions } from "./types/Fetch";
 import { version } from "./version";
 
-export const NODE_VERSION = process?.versions.node || "Unknown";
+export const NODE_VERSION = process?.versions?.node || "Unknown";
 
 export const DEFAULT_HEADERS = {
   "Content-Type": `application/json`,
