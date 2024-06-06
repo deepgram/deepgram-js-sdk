7ce44bdd639891b06bfde6385a815372a5122eee
Richard <richard@stromer.org>
2024-06-06 16:20:39 +0200

    fix: fallback from undefined process variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,10 @@ import { DeepgramClientOptions } from "./types/DeepgramClientOptions";
 import { FetchOptions } from "./types/Fetch";
 import { version } from "./version";
 
-export const NODE_VERSION = process?.versions?.node || "Unknown";
+export const NODE_VERSION =
+  typeof process === "undefined"
+    ? "Unknown"
+    : process?.versions?.node || "Unknown";
 
 export const DEFAULT_HEADERS = {
   "Content-Type": `application/json`,
