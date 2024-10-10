753be7ee4ed181653d036e662872f87359390bda
Naomi Carrigan <commits@nhcarrigan.com>
2024-10-10 12:08:13 -0700

    fix: pass sample rate option

--- examples/node-speak-live/index.js
+++ examples/node-speak-live/index.js
@@ -26,7 +26,11 @@ const live = async () => {
 
   const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 
-  const dgConnection = deepgram.speak.live({ model: "aura-asteria-en", encoding: "linear16" });
+  const dgConnection = deepgram.speak.live({
+    model: "aura-asteria-en",
+    encoding: "linear16",
+    sample_rate: 48000,
+  });
 
   let audioBuffer = Buffer.from(wavHeader);
 
