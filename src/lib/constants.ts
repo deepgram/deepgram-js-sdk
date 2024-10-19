a40604a09e99836be18017a9d596444e8d2bcc89
Naomi Carrigan <commits@nhcarrigan.com>
2024-10-18 17:52:34 -0700

    feat: release voice agent
    feat: finish it up!
    
    fix: oops
    
    Co-authored-by: Luke Oliff <luke@lukeoliff.com>
    
    feat: use agent directly, no live prop
    
    feat: configurable websocket url
    
    fix: can't import with extensions 😩
    
    wip: browser example
    
    feat: i feel like i'm getting nowhere here
    
    The fact that the websocket doesn't say *anything* back is really
    not helpful.
    
    chore: add warning about browser
    
    feat: update for ga
    
    fix: expand example

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -36,6 +36,7 @@ export const DEFAULT_HEADERS = {
 };
 
 export const DEFAULT_URL = "https://api.deepgram.com";
+export const DEFAULT_AGENT_URL = "wss://agent.deepgram.com";
 
 export const DEFAULT_GLOBAL_OPTIONS: Partial<DefaultNamespaceOptions> = {
   fetch: { options: { url: DEFAULT_URL, headers: DEFAULT_HEADERS } },
@@ -44,8 +45,16 @@ export const DEFAULT_GLOBAL_OPTIONS: Partial<DefaultNamespaceOptions> = {
   },
 };
 
+export const DEFAULT_AGENT_OPTIONS: Partial<DefaultNamespaceOptions> = {
+  fetch: { options: { url: DEFAULT_URL, headers: DEFAULT_HEADERS } },
+  websocket: {
+    options: { url: DEFAULT_AGENT_URL, _nodeOnlyHeaders: DEFAULT_HEADERS },
+  },
+};
+
 export const DEFAULT_OPTIONS: DefaultClientOptions = {
   global: DEFAULT_GLOBAL_OPTIONS,
+  agent: DEFAULT_AGENT_OPTIONS,
 };
 
 export enum SOCKET_STATES {
--- src/packages/index.ts
+++ src/packages/index.ts
@@ -1,6 +1,7 @@
 export * from "./AbstractClient";
 export * from "./AbstractLiveClient";
 export * from "./AbstractRestClient";
+export * from "./AgentLiveClient";
 export * from "./ListenClient";
 export * from "./ListenLiveClient";
 export * from "./ListenRestClient";
