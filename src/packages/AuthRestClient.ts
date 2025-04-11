f95597afcae6302679697e3299384283dd15f312
Naomi Carrigan <commits@nhcarrigan.com>
2025-04-11 16:02:29 -0700

    chore: short-lived -> temporary

--- src/packages/AuthRestClient.ts
+++ src/packages/AuthRestClient.ts
@@ -6,6 +6,11 @@ import { AbstractRestClient } from "./AbstractRestClient";
 export class AuthRestClient extends AbstractRestClient {
   public namespace: string = "auth";
 
+  /**
+   * Generates a new temporary token for the Deepgram API.
+   * @param endpoint Optional custom endpoint to use for the request. Defaults to ":version/auth/grant".
+   * @returns Object containing the result of the request or an error if one occurred. Result will contain access_token and expires_in properties.
+   */
   public async grantToken(
     endpoint = ":version/auth/grant"
   ): Promise<DeepgramResponse<GrantTokenResponse>> {
