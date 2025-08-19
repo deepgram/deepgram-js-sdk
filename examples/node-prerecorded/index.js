83061184ace6a52bc77d9cd1f3f79ffb4ab34b0a
Evan Henry <evan.henry@deepgram.com>
2025-08-19 13:06:48 -0400

    Added the EU endpoint in the `node-prerecorded` example, does not seem to be working for a few of the tests

--- examples/node-prerecorded/index.js
+++ examples/node-prerecorded/index.js
@@ -10,15 +10,24 @@ const transcribeUrlAuthWithFactory = async () => {
   console.log("transcribing url with auth factory");
 
   const authFactory = async () => {
-    const deepgramTokenClient = createClient(process.env.DEEPGRAM_API_KEY);
+    const deepgramTokenClient = createClient(process.env.DEEPGRAM_API_KEY, {
+      global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+    });
 
     const { result: tokenResult, error: tokenError } = await deepgramTokenClient.auth.grantToken();
+
+    if (error) console.error(error);
 
     if (tokenError) console.error(tokenError);
     if (!tokenError) return tokenResult.access_token;
   };
 
-  const deepgramClient = createClient({ accessToken: authFactory });
+  const deepgramClient = createClient(
+    { accessToken: authFactory },
+    {
+      global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+    }
+  );
 
   console.log("Transcribing URL", "https://dpgr.am/spacewalk.wav");
   const { result, error } = await deepgramClient.listen.prerecorded.transcribeUrl(
@@ -32,18 +41,32 @@ const transcribeUrlAuthWithFactory = async () => {
 
   if (error) console.error(error);
   if (!error) console.dir(result, { depth: 1 });
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result.results.channels[0].alternatives[0].transcript);
+  }
 };
 
 const transcribeUrlWithAccessToken = async () => {
   console.log("transcribing url with access token");
 
-  const deepgramTokenClient = createClient(process.env.DEEPGRAM_API_KEY);
+  const deepgramTokenClient = createClient(process.env.DEEPGRAM_API_KEY, {
+    global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+  });
 
   const { result: tokenResult, error: tokenError } = await deepgramTokenClient.auth.grantToken();
 
+  console.log("tokenResult: " + tokenResult);
+  console.log("tokenError: " + tokenError);
   if (tokenError) console.error(tokenError);
 
-  const deepgramClient = createClient({ accessToken: tokenResult.access_token });
+  const deepgramClient = createClient(
+    { accessToken: tokenResult.access_token },
+    {
+      global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+    }
+  );
 
   console.log("Transcribing URL", "https://dpgr.am/spacewalk.wav");
   const { result, error } = await deepgramClient.listen.prerecorded.transcribeUrl(
@@ -57,12 +80,19 @@ const transcribeUrlWithAccessToken = async () => {
 
   if (error) console.error(error);
   if (!error) console.dir(result, { depth: 1 });
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result.results.channels[0].alternatives[0].transcript);
+  }
 };
 
 const transcribeUrl = async () => {
   console.log("transcribing url with api key");
 
-  const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+  const deepgram = createClient(process.env.DEEPGRAM_API_KEY, {
+    global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+  });
 
   console.log("Transcribing URL", "https://dpgr.am/spacewalk.wav");
   const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
@@ -76,12 +106,19 @@ const transcribeUrl = async () => {
 
   if (error) console.error(error);
   if (!error) console.dir(result, { depth: 1 });
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result.results.channels[0].alternatives[0].transcript);
+  }
 };
 
 const transcribeFile = async () => {
   console.log("transcribing file with api key");
 
-  const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+  const deepgram = createClient(process.env.DEEPGRAM_API_KEY, {
+    global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+  });
 
   const filePath = resolve(__dirname, "../spacewalk.wav");
   console.log(filePath);
@@ -94,10 +131,17 @@ const transcribeFile = async () => {
 
   if (error) console.error(error);
   if (!error) console.dir(result, { depth: 1 });
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result.results.channels[0].alternatives[0].transcript);
+  }
 };
 
 const transcribe = async () => {
-  const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+  const deepgram = createClient(process.env.DEEPGRAM_API_KEY, {
+    global: { fetch: { options: { url: "https://api.eu.deepgram.com" } } },
+  });
   const audio = readFileSync(resolve(__dirname, "../spacewalk.wav"));
 
   const { result, error } = await deepgram.listen.prerecorded.transcribeFile(audio, {
@@ -112,9 +156,9 @@ const transcribe = async () => {
 };
 
 (async () => {
-  await transcribeUrl();
-  await transcribeFile();
+  //await transcribeUrl();
+  //await transcribeFile();
   await transcribeUrlAuthWithFactory();
   await transcribeUrlWithAccessToken();
-  await transcribe();
+  //await transcribe();
 })();
