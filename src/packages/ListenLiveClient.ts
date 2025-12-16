b8bb533e28a4f001b34f44248d400bfa3f4e9739
Luke Oliff <luke@lukeoliff.com>
2025-12-16 16:10:37 +0000

    fix: remove keyterm model condition from ListenLiveClient.ts

--- src/packages/ListenLiveClient.ts
+++ src/packages/ListenLiveClient.ts
@@ -35,10 +35,6 @@ export class ListenLiveClient extends AbstractLiveClient {
   ) {
     super(options);
 
-    if (transcriptionOptions.keyterm?.length && !transcriptionOptions.model?.startsWith("nova-3") && !transcriptionOptions.model?.startsWith("flux")) {
-      throw new DeepgramError("Keyterms are only supported with the Nova 3 and Flux models.");
-    }
-
     this.connect(transcriptionOptions, endpoint);
   }
 
